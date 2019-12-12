8c7ddeb4fc691f1c72d4e7564450dd6ac6b3e975
User <User@KALL>
2019-12-11 21:01:49 -0500

    validacion

--- src/Formularios/frmPrestamoAdelantoSuel.java
+++ src/Formularios/frmPrestamoAdelantoSuel.java
@@ -5,6 +5,9 @@
  */
 package Formularios;
 
+import java.awt.Toolkit;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Ordenador
@@ -16,6 +19,7 @@ public class frmPrestamoAdelantoSuel extends javax.swing.JFrame {
      */
     public frmPrestamoAdelantoSuel() {
         initComponents();
+        setLocationRelativeTo(null);
     }
 
     /**
@@ -33,12 +37,12 @@ public class frmPrestamoAdelantoSuel extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jTextField5 = new javax.swing.JTextField();
-        jTextField6 = new javax.swing.JTextField();
+        txtCCliente = new javax.swing.JTextField();
+        txtNCuotas = new javax.swing.JTextField();
+        txtSCliente = new javax.swing.JTextField();
+        txtCEmpleado = new javax.swing.JTextField();
+        txtNTarjeta = new javax.swing.JTextField();
+        txtMSoli = new javax.swing.JTextField();
         btnGrabar = new javax.swing.JButton();
         btnActualizar = new javax.swing.JButton();
         btnBuscar = new javax.swing.JButton();
@@ -63,6 +67,42 @@ public class frmPrestamoAdelantoSuel extends javax.swing.JFrame {
         jLabel5.setText("Número de Tarjeta:");
 
         jLabel6.setText("Monto a Solicitar:");
+
+        txtCCliente.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtCClienteKeyTyped(evt);
+            }
+        });
+
+        txtNCuotas.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtNCuotasKeyTyped(evt);
+            }
+        });
+
+        txtSCliente.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtSClienteKeyTyped(evt);
+            }
+        });
+
+        txtCEmpleado.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtCEmpleadoKeyTyped(evt);
+            }
+        });
+
+        txtNTarjeta.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtNTarjetaKeyTyped(evt);
+            }
+        });
+
+        txtMSoli.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtMSoliKeyTyped(evt);
+            }
+        });
 
         btnGrabar.setText("Grabar");
 
@@ -103,23 +143,27 @@ public class frmPrestamoAdelantoSuel extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(txtNTarjeta, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtSCliente, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtCEmpleado, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtCCliente, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(39, 39, 39)
+                                .addComponent(txtNCuotas, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jTextField6)))
+                        .addComponent(txtMSoli)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -145,7 +189,7 @@ public class frmPrestamoAdelantoSuel extends javax.swing.JFrame {
                         .addComponent(btnNuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(btnActualizar, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(45, Short.MAX_VALUE))
+                .addContainerGap(36, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1)
@@ -157,7 +201,7 @@ public class frmPrestamoAdelantoSuel extends javax.swing.JFrame {
                 .addGap(20, 20, 20)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField1)
+                    .addComponent(txtCCliente)
                     .addComponent(btnActualizar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnNuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -165,27 +209,27 @@ public class frmPrestamoAdelantoSuel extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField4))
+                            .addComponent(txtCEmpleado))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField2))
+                            .addComponent(txtNCuotas))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField3)
+                            .addComponent(txtSCliente)
                             .addComponent(btnBuscar)
                             .addComponent(btnEliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(txtNTarjeta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addComponent(btnListar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtMSoli, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel7)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(29, 29, 29)
@@ -200,6 +244,92 @@ public class frmPrestamoAdelantoSuel extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtCClienteKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtCClienteKeyTyped
+        char c = evt.getKeyChar();
+        
+        if(txtCCliente.getText().length()>= 6 ){
+            JOptionPane.showMessageDialog(null, "Limite de Caracteres");
+            evt.consume();
+           }
+         if(c<'0' || c>'9'){
+            JOptionPane.showMessageDialog(null, "No se Aceptan Letras");
+            evt.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
+    }//GEN-LAST:event_txtCClienteKeyTyped
+
+    private void txtCEmpleadoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtCEmpleadoKeyTyped
+        if(txtCEmpleado.getText().length()>= 6){
+            JOptionPane.showMessageDialog(null, "Limite de Caracteres");
+            evt.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
+        char c = evt.getKeyChar();
+        if(c<'0' || c>'9'){
+            JOptionPane.showMessageDialog(null, "No se Aceptan Letras");
+            evt.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
+    }//GEN-LAST:event_txtCEmpleadoKeyTyped
+
+    private void txtNCuotasKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtNCuotasKeyTyped
+        if(txtNCuotas.getText().length()>= 2){
+            evt.consume();
+            JOptionPane.showMessageDialog(null, "Limite de Caracteres");
+            Toolkit.getDefaultToolkit().beep();
+            
+        }
+        
+        char c = evt.getKeyChar();
+        if(c<'0' || c>'9'){
+            JOptionPane.showMessageDialog(null, "No se Aceptan Letras");
+            evt.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
+    }//GEN-LAST:event_txtNCuotasKeyTyped
+
+    private void txtSClienteKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtSClienteKeyTyped
+        if(txtSCliente.getText().length()>= 7){
+            evt.consume();
+            JOptionPane.showMessageDialog(null, "Limite de Caracteres");
+            Toolkit.getDefaultToolkit().beep();
+        }
+        char c = evt.getKeyChar();
+        if(c<'0' || c>'9'){
+            JOptionPane.showMessageDialog(null, "No se Aceptan Letras");
+            evt.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
+    }//GEN-LAST:event_txtSClienteKeyTyped
+
+    private void txtNTarjetaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtNTarjetaKeyTyped
+        if(txtNTarjeta.getText().length()>= 19){
+            evt.consume();
+            JOptionPane.showMessageDialog(null, "Limite de Caracteres");
+            Toolkit.getDefaultToolkit().beep();
+        }
+        char c = evt.getKeyChar();
+        if(c<'0' || c>'9'){
+            JOptionPane.showMessageDialog(null, "No se Aceptan Letras");
+            evt.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
+    }//GEN-LAST:event_txtNTarjetaKeyTyped
+
+    private void txtMSoliKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtMSoliKeyTyped
+        if(txtMSoli.getText().length()>= 7){
+            evt.consume();
+            JOptionPane.showMessageDialog(null, "Limite de Caracteres");
+            Toolkit.getDefaultToolkit().beep();
+        }
+        char c = evt.getKeyChar();
+        if(c<'0' || c>'9'){
+            JOptionPane.showMessageDialog(null, "No se Aceptan Letras");
+            evt.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
+    }//GEN-LAST:event_txtMSoliKeyTyped
 
     /**
      * @param args the command line arguments
@@ -253,11 +383,11 @@ public class frmPrestamoAdelantoSuel extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField txtCCliente;
+    private javax.swing.JTextField txtCEmpleado;
+    private javax.swing.JTextField txtMSoli;
+    private javax.swing.JTextField txtNCuotas;
+    private javax.swing.JTextField txtNTarjeta;
+    private javax.swing.JTextField txtSCliente;
     // End of variables declaration//GEN-END:variables
 }
